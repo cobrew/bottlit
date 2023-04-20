f9e3a103c2ccc27461390858cd2be80134c44d9c
Jeff Schiller <codedread@gmail.com>
2023-04-19 21:12:24 -0700

    Starter yeast for bottlit (Preact boilerplate)

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import { Component, RenderableProps, h, render } from 'preact';
+
+interface AppProps {
+  name: string;
+}
+
+interface AppState {
+  count: number;
+}
+
+export class App extends Component<AppProps, AppState> {
+  state: AppState = { count: 0 };
+
+  clicky(name: string) {
+    const newCount = this.state.count + 1;
+    alert(`Hello, ${name}! You clicked ${newCount} times.`);
+    this.setState({count: newCount});
+  }
+
+  render(props: AppProps) {
+    return (
+      <div>
+        <button onClick={() => this.clicky(props.name)}>
+          Clicky
+        </button>
+        <div>(clicked {this.state.count} times)</div>
+      </div>
+    );
+  }
+}
+
+render(<App name="Jeff"/>, document.getElementById("app"));
