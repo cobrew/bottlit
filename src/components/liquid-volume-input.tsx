1b38cab3ea00c0fc319a06f49ede6200ffaaa171
Jeff Schiller <codedread@gmail.com>
2024-04-07 22:30:02 -0700

    Fix bug when trying to enter decimals for volume input.

--- src/components/liquid-volume-input.tsx
+++ src/components/liquid-volume-input.tsx
@@ -11,22 +11,43 @@ interface LiquidVolumeProps {
 }
 
 interface LiquidVolumeState {
+  /** Only set when an invalid string is present. */
+  errValue_L: string;
+  errValue_OZ: string;
   volume: LiquidVolume;
 }
 
-export class LiquidVolumeInput extends Component<LiquidVolumeProps, LiquidVolumeState> {
-  state = { volume: new LiquidVolume(0) };
+const VALID_NUM_REGEX = new RegExp('^(([0-9]+)|([0-9]+[.][0-9]+))$')
 
-  handleChangeInput(e: Event, unitType: UnitDescriptor, props: LiquidVolumeProps) {
+/** A UI component for liquid volume that has two linked inputs representing Litres and Ounces. */
+export class LiquidVolumeInput extends Component<LiquidVolumeProps, LiquidVolumeState> {
+  state = { volume: new LiquidVolume(0), errValue_L: undefined, errValue_OZ: undefined };
+
+  handleInput(e: Event, unitType: UnitDescriptor, props: LiquidVolumeProps) {
     const inputEl = e.target as HTMLInputElement;
-    const val = new LiquidVolume(inputEl.valueAsNumber, unitType);
-    this.setState({
-      volume: val,
-    });
-    if (props.updateQuantityFn) {
-      props.updateQuantityFn(val);
+    const amt = inputEl.value;
+    if (!amt || amt.match(VALID_NUM_REGEX)) {
+      this.handleChange(e, unitType, props);
+    } else {
+      if (unitType.description === 'L') {
+        this.setState({errValue_L: amt});
+      } else {
+        this.setState({errValue_OZ: amt});
+      }
     }
   };
+
+  handleChange(e: Event, unitType: UnitDescriptor, props: LiquidVolumeProps) {
+    const inputEl = e.target as HTMLInputElement;
+    const amt = inputEl.value;
+    if (!amt || amt.match(VALID_NUM_REGEX)) {
+      const val = new LiquidVolume(Number(amt), unitType);
+      this.setState({ volume: val, errValue_L: undefined, errValue_OZ: undefined });
+      if (props.updateQuantityFn) {
+        props.updateQuantityFn(val);
+      }
+    }
+  }
 
   handleFocus(evt: Event) {
     const inputEl = evt.target as HTMLInputElement;
@@ -38,20 +59,22 @@ export class LiquidVolumeInput extends Component<LiquidVolumeProps, LiquidVolume
       <div>
         <label>{props.labelString}:</label>
         <input
-          type="number"
+          type="text"
+          class={this.state.errValue_L ? "err" : ""}
           disabled={!props.editable}
-          value={this.props.editable ? this.state.volume.get(LITRE) : this.props.volume.get(LITRE)}
+          value={this.props.editable ? (this.state.errValue_L ?? this.state.volume.get(LITRE)) : this.props.volume.get(LITRE)}
           onFocus={this.handleFocus}
-          onInput={(evt) => this.handleChangeInput(evt, LITRE, props)}
+          onInput={(evt) => this.handleInput(evt, LITRE, props)}
           placeholder="Litres"
         />
         <span> liters</span>
         <input
-          type="number"
+          type="text"
+          class={this.state.errValue_OZ ? "err" : ""}
           disabled={!props.editable}
-          value={this.props.editable ? this.state.volume.get(OZ) : this.props.volume.get(OZ)}
+          value={this.props.editable ? (this.state.errValue_OZ ?? this.state.volume.get(OZ)) : this.props.volume.get(OZ)}
           onFocus={this.handleFocus}
-          onInput={(evt) => this.handleChangeInput(evt, OZ, props)}
+          onInput={(evt) => this.handleInput(evt, OZ, props)}
           placeholder="Ounces"
         />
         <span> ounces</span>
